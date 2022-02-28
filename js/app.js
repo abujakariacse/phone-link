97a6395d6c9b6682a48e8f25616677f37dbc2ff1
abujakariacse <abujakaria316@gmail.com>
2022-03-01 02:56:26 +0600

    details_show

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ const searchPhone = () =>{
 
 }
 
+// Search Result
 const ShowPhone = phones =>{
     const showResult = document.getElementById('search-result');
+    showResult.textContent = '';    
     phones.forEach(phone => {
-        console.log(phone);
-
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -23,13 +23,47 @@ const ShowPhone = phones =>{
         <img src="${phone.image}" class="card-img-top" alt="...">
         </div>
         <div class="card-body">
-            <h3 class="card-title text-center">${phone.phone_name}</h3>
-            <h4 class="card-text text-center text-primary">${phone.brand}</h4>
-            <div class="d-flex justify-content-center btn-lg"><button type="button" class="btn btn-primary btn-lg">Details</button></div>
+            <h3 class="card-title text-center">Model: ${phone.phone_name.slice(0,20)}</h3>
+            <h4 class="card-text text-center text-primary">Brand: ${phone.brand}</h4>
+            <div class="d-flex justify-content-center btn-lg">
+            <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-primary btn-lg">Details</button>
+            </div>
         </div>
         
     </div>
         `;
         showResult.appendChild(div);
     });
+}
+
+// Phone Details
+const phoneDetails = id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => moreInfo(data.data))
+};
+
+const moreInfo = data =>{
+    console.log(data);
+    const details = document.getElementById('phone-details');
+    details.textContent='';
+    details.style.backgroundColor = '#fff7e0a1';
+    const PhoneImg = document.createElement('div');
+    PhoneImg.classList.add('child');
+    PhoneImg.innerHTML = `
+    <image src="${data.image}">
+    `;
+    const phoneDetails = document.createElement('div');
+    phoneDetails.classList.add('child')
+    phoneDetails.innerHTML = `
+    <h4>Name: ${data.name}</h4>
+    <h5>Realease Date: ${data.releaseDate}</h5>
+    <h4 class="fw-bold">Main Features</h4>
+    <h5>Chipset: ${data.mainFeatures.chipSet}</h5>
+    <h5>Display: ${data.mainFeatures.displaySize}</h5>
+    <h5>Memory: ${data.mainFeatures.memory}</h5>
+    `;
+    details.appendChild(PhoneImg);
+    details.appendChild(phoneDetails);
 }
