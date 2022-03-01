3603b6b7e0d5e72bf5bac1aa2cb172e123c86202
abujakariacse <abujakaria316@gmail.com>
2022-03-02 01:19:43 +0600

    bonus added

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ const ShowPhone = phones =>{
        }
        
        else{
-         phones.forEach(phone => {
+         phones.slice(0,20).forEach(phone => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -46,11 +46,11 @@ const ShowPhone = phones =>{
             <div class="w-50 mx-auto">
             <img src="${phone.image}" class="card-img-top" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body alert-secondary">
                 <h3 class="card-title text-center">${phone.phone_name.slice(0,20)}</h3>
                 <h4 class="card-text text-center text-primary">${phone.brand}</h4>
                 <div class="d-flex justify-content-center btn-lg">
-                <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-primary btn-lg">Details</button>
+                <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-lg btn-primary">Details</button>
                 </div>
             </div> 
         </div>`;
@@ -68,10 +68,10 @@ const ShowPhone = phones =>{
     
     // Details About Phone
     const moreDetails = data =>{
+        console.log(data);
         if(data.releaseDate != 0 && data.others !=0 && data.slug != 'apple_iphone_13_mini-11104'){
         const details =  document.getElementById('phone-details');
         details.textContent='';
-        details.style.backgroundColor = '#fff7e0a1';
         const PhoneImg = document.createElement('div');
         PhoneImg.classList.add('child');
         PhoneImg.innerHTML = `
@@ -79,25 +79,31 @@ const ShowPhone = phones =>{
         const phoneDetails = document.createElement('div');
         phoneDetails.classList.add('child');
         phoneDetails.innerHTML = `
-        <h5>Name: ${data.name}</h5>
-        <h5>Realease Date: ${data.releaseDate}</h5>
+        <h5 class="fw-bold">About Phone:-</h5>
+        <h6>Name: ${data.name}</h6>
+        <h6>Realease Date: ${data.releaseDate}</h6>
+        <hr>
         <h5 class="fw-bold">Main Features:-</h5>
-        <h5>Chipset: ${data.mainFeatures.chipSet}</h5>
-        <h5>Display: ${data.mainFeatures.displaySize}</h5>
-        <h5>Memory: ${data.mainFeatures.memory}</h5>
-        <h5>Bluetooth: ${data.others.Bluetooth} </h5>
-        <h5>GPS: ${data.others.GPS} </h5>
-        <h5>NFC: ${data.others.NFC} </h5>
-        <h5>Radio: ${data.others.Radio} </h5>
-        <h5>USB: ${data.others.USB} </h5>
-        <h5>WLAN: ${data.others.WLAN} </h5>
+        <h6>Chipset: ${data.mainFeatures.chipSet}</h6>
+        <h6>Storage: ${data.mainFeatures.storage}</h6>
+        <hr>
+        <h6>Display: ${data.mainFeatures.displaySize}</h6>
+        <h6>Memory: ${data.mainFeatures.memory}</h6>
+        <hr>
+        <h6>Bluetooth: ${data.others.Bluetooth} </h6>
+        <h6>GPS: ${data.others.GPS} </h6>
+        <h6>NFC: ${data.others.NFC} </h6>
+        <hr>
+        <h6>Radio: ${data.others.Radio} </h6>
+        <h6>USB: ${data.others.USB} </h6>
+        <h6>WLAN: ${data.others.WLAN} </h6>
+        <hr>
         <h5 class="fw-bold">Sensors:-</h5>
-        <h5>1.${data.mainFeatures.sensors[0]} </h5>
-        <h5>2.${data.mainFeatures.sensors[1]} </h5>
-        <h5>3.${data.mainFeatures.sensors[2]} </h5>
-        <h5>4.${data.mainFeatures.sensors[3]} </h5>
-        <h5>5.${data.mainFeatures.sensors[4]} </h5>
-        <h5>6.${data.mainFeatures.sensors[5]} </h5>`;
+        <h6>
+        ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},
+        ${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},
+        ${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}
+        </h6>`;
         details.appendChild(PhoneImg);
         details.appendChild(phoneDetails);
         }
@@ -105,7 +111,6 @@ const ShowPhone = phones =>{
     else if(data.slug != 'apple_iphone_13_mini-11104' && data.others != 0){
         const details =  document.getElementById('phone-details');
         details.textContent='';
-        details.style.backgroundColor = '#fff7e0a1';
         const PhoneImg = document.createElement('div');
         PhoneImg.classList.add('child');
         PhoneImg.innerHTML = `
@@ -113,25 +118,31 @@ const ShowPhone = phones =>{
         const phoneDetails = document.createElement('div');
         phoneDetails.classList.add('child')
         phoneDetails.innerHTML = `
-        <h5>Name: ${data.name}</h5>
-        <h5>Realease Date: Not Publish Yet</h5>
+        <h5 class="fw-bold">About Phone:-</h5>
+        <h6>Name: ${data.name}</h6>
+        <h6>Realease Date: Not Found!</h6>
+        <hr>
         <h5 class="fw-bold">Main Features:-</h5>
-        <h5>Chipset: ${data.mainFeatures.chipSet}</h5>
-        <h5>Display: ${data.mainFeatures.displaySize}</h5>
-        <h5>Memory: ${data.mainFeatures.memory}</h5>
-        <h5>Bluetooth: ${data.others.Bluetooth} </h5>
-        <h5>GPS: ${data.others.GPS} </h5>
-        <h5>NFC: ${data.others.NFC} </h5>
-        <h5>Radio: ${data.others.Radio} </h5>
-        <h5>USB: ${data.others.USB} </h5>
-        <h5>WLAN: ${data.others.WLAN} </h5>
+        <h6>Chipset: ${data.mainFeatures.chipSet}</h6>
+        <h6>Storage: ${data.mainFeatures.storage}</h6>
+        <hr>
+        <h6>Display: ${data.mainFeatures.displaySize}</h6>
+        <h6>Memory: ${data.mainFeatures.memory}</h6>
+        <hr>
+        <h6>Bluetooth: ${data.others.Bluetooth} </h6>
+        <h6>GPS: ${data.others.GPS} </h6>
+        <h6>NFC: ${data.others.NFC} </h6>
+        <hr>
+        <h6>Radio: ${data.others.Radio} </h6>
+        <h6>USB: ${data.others.USB} </h6>
+        <h6>WLAN: ${data.others.WLAN} </h6>
+        <hr>
         <h5 class="fw-bold">Sensors:-</h5>
-        <h5>1.${data.mainFeatures.sensors[0]} </h5>
-        <h5>2.${data.mainFeatures.sensors[1]} </h5>
-        <h5>3.${data.mainFeatures.sensors[2]} </h5>
-        <h5>4.${data.mainFeatures.sensors[3]} </h5>
-        <h5>5.${data.mainFeatures.sensors[4]} </h5>
-        <h5>6.${data.mainFeatures.sensors[5]} </h5>`;
+        <h6>
+        ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},
+        ${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},
+        ${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}
+        </h6>`;
         details.appendChild(PhoneImg);
         details.appendChild(phoneDetails);
         }
@@ -139,7 +150,6 @@ const ShowPhone = phones =>{
         else if(data.releaseDate != 0 && data.others !=0){
             const details =  document.getElementById('phone-details');
             details.textContent='';
-            details.style.backgroundColor = '#fff7e0a1';
             const PhoneImg = document.createElement('div');
             PhoneImg.classList.add('child');
             PhoneImg.innerHTML = `
@@ -147,34 +157,33 @@ const ShowPhone = phones =>{
             const phoneDetails = document.createElement('div');
             phoneDetails.classList.add('child')
             phoneDetails.innerHTML = `
-            <h5>Name: ${data.name}</h5>
-            <h5>Realease Date: ${data.releaseDate}</h5>
-            <h5 class="fw-bold">Main Features:-</h5>
-            <h5>Chipset: ${data.mainFeatures.chipSet}</h5>
-            <h5>Display: ${data.mainFeatures.displaySize}</h5>
-            <h5>Memory: ${data.mainFeatures.memory}</h5>
-            <h5>Bluetooth: Not Available</h5>
-            <h5>GPS: Not Available </h5>
-            <h5>NFC: Not Available </h5>
-            <h5>Radio: Not Available </h5>
-            <h5>USB: Not Available </h5>
-            <h5>WLAN: Not Available </h5>
-            <h5 class="fw-bold">Sensors:-</h5>
-            <h5>1.${data.mainFeatures.sensors[0]} </h5>
-            <h5>2.${data.mainFeatures.sensors[1]} </h5>
-            <h5>3.${data.mainFeatures.sensors[2]} </h5>
-            <h5>4.${data.mainFeatures.sensors[3]} </h5>
-            <h5>5.${data.mainFeatures.sensors[4]} </h5>
-            <h5>6.${data.mainFeatures.sensors[5]} </h5>`;
+            <h5 class="fw-bold">About Phone:-</h5>
+        <h6>Name: ${data.name}</h6>
+        <h6>Realease Date: ${data.releaseDate}</h6>
+        <hr>
+        <h5 class="fw-bold">Main Features:-</h5>
+        <h6>Chipset: ${data.mainFeatures.chipSet}</h6>
+        <h6>Storage: ${data.mainFeatures.storage}</h6>
+        <hr>
+        <h6>Display: ${data.mainFeatures.displaySize}</h6>
+        <h6>Memory: ${data.mainFeatures.memory}</h6>
+        <hr>
+        <h6>Bluetooth: Not Available</h6>
+        <h6>GPS: Not Available</h6>
+        <h6>NFC: Not Available</h6>
+        <hr>
+        <h6>Radio: Not Available</h6>
+        <h6>USB: Not Available</h6>
+        <h6>WLAN: Not Available</h6>
+        <hr>
+        <h5 class="fw-bold">Sensors:-</h5>
+        <h6>
+        ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},
+        ${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},
+        ${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}
+        </h6>`;
             details.appendChild(PhoneImg);
             details.appendChild(phoneDetails);
             
     }
 }
-
-
-
-
-
-
-
